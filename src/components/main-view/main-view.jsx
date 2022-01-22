4ab9b22183c374a9a542c4484d96b321041facac
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-01-21 22:28:38 -0800

    Update main-view.jsx
    removed the list is empty text

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -36,7 +36,7 @@ export class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
     
-    if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+    if (movies.length === 0) return <div className="main-view"></div>;
   
     return (
       <div className="main-view">

a1b0de70ead342222b2f282d0976a5ade92734e3
MattE55 <89932886+MattE55@users.noreply.github.com>
2022-01-22 20:58:38 -0800

    main view update
    updated main view jsx to work with the login view

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,10 +9,9 @@ export class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
-      movies: [
-
-      ],
-      selectedMovie: null
+      movies: [ ],
+      selectedMovie: null,
+      user: null
     };
   }
 
@@ -33,9 +33,18 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedIn(user) {
+    this.setState({
+      user
+    });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state;
+    const { movies, selectedMovie, user } = this.state;
     
+    /* If there is no user, the LoginView is rendered. If there is a user logged in, the user details are *passed as a prop to the LoginView*/
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
     if (movies.length === 0) return <div className="main-view"></div>;
   
     return (
@@ -52,8 +61,8 @@ export class MainView extends React.Component {
             <MovieCard
               key={movie._id}
               movie={movie}
-              onMovieClick={(movie) => {
-                this.setSelectedMovie(movie);
+              onMovieClick={(newSelectedMovie) => {
+                this.setSelectedMovie(newSelectedMovie);
               }}
             />
           ))
